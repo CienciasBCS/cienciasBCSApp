a74bd0beafd79a080d83774fe1e873bc64ff0ba7
René Fernando <renefernandoortega@gmail.com>
2021-08-23 14:23:19 -0600

    Updates

---
+++ precache-manifest.99f813fc5ecef4ac4e9056ba2e6ad458.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4c8b11aa73daede1f9e0a32255ffc4",
+    "url": "/cienciasBCSApp/index.html"
+  },
+  {
+    "revision": "ee2db672c350abf4d3e6",
+    "url": "/cienciasBCSApp/static/css/2.1e0304ae.chunk.css"
+  },
+  {
+    "revision": "8981f7192b0890137ff1",
+    "url": "/cienciasBCSApp/static/css/main.6810c0bb.chunk.css"
+  },
+  {
+    "revision": "ee2db672c350abf4d3e6",
+    "url": "/cienciasBCSApp/static/js/2.6bca53a0.chunk.js"
+  },
+  {
+    "revision": "8981f7192b0890137ff1",
+    "url": "/cienciasBCSApp/static/js/main.d940a05b.chunk.js"
+  },
+  {
+    "revision": "6ca03aeb192e8ffea125",
+    "url": "/cienciasBCSApp/static/js/runtime~main.29831e87.js"
+  },
+  {
+    "revision": "40857786deec754340d455629c463325",
+    "url": "/cienciasBCSApp/static/media/Flaticon.40857786.woff2"
+  },
+  {
+    "revision": "4be0b1dd8601b81b9731d1ce50290c97",
+    "url": "/cienciasBCSApp/static/media/Flaticon.4be0b1dd.eot"
+  },
+  {
+    "revision": "7b6c68f0263dd7993b6ce2bd5333ef50",
+    "url": "/cienciasBCSApp/static/media/Flaticon.7b6c68f0.svg"
+  },
+  {
+    "revision": "c874e5306245645be2468e42249f6815",
+    "url": "/cienciasBCSApp/static/media/Flaticon.c874e530.woff"
+  },
+  {
+    "revision": "db9a5fdf3602d76377c26933fccdb150",
+    "url": "/cienciasBCSApp/static/media/Flaticon.db9a5fdf.ttf"
+  },
+  {
+    "revision": "65e782dd74bdc0ad5bbc532daccefa2d",
+    "url": "/cienciasBCSApp/static/media/Now-Bold.65e782dd.ttf"
+  },
+  {
+    "revision": "3a0b6dbce353d3a1d773c85db41cacd8",
+    "url": "/cienciasBCSApp/static/media/bg.3a0b6dbc.png"
+  },
+  {
+    "revision": "73a4c602d5d15bb6f7ce5069790fb4bd",
+    "url": "/cienciasBCSApp/static/media/bg.73a4c602.png"
+  },
+  {
+    "revision": "b6a638ae9704f8bead4ea5aea7c32b44",
+    "url": "/cienciasBCSApp/static/media/bg2.b6a638ae.png"
+  },
+  {
+    "revision": "5756a636c533a7c8bb3b741dcd1988c0",
+    "url": "/cienciasBCSApp/static/media/en-construccion.5756a636.jpg"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/cienciasBCSApp/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/cienciasBCSApp/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "a02519e3436711cf1ef1a7348b8cdbe2",
+    "url": "/cienciasBCSApp/static/media/fontawesome-webfont.a02519e3.svg"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/cienciasBCSApp/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/cienciasBCSApp/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "9507a037d06eb23fcb282cf7e0954c63",
+    "url": "/cienciasBCSApp/static/media/now-light.9507a037.otf"
+  },
+  {
+    "revision": "a52f54559b2cb2642ebf886d28c79a01",
+    "url": "/cienciasBCSApp/static/media/now-medium.a52f5455.otf"
+  },
+  {
+    "revision": "43bf8f54b4f539bed0c9048b90c428b2",
+    "url": "/cienciasBCSApp/static/media/now-regular.43bf8f54.otf"
+  },
+  {
+    "revision": "894eaf68e2d41c0581ed66377f7137c1",
+    "url": "/cienciasBCSApp/static/media/now-thin.894eaf68.otf"
+  },
+  {
+    "revision": "e6eb1a1119945a3a2645c3a97f4d1517",
+    "url": "/cienciasBCSApp/static/media/paymentbg.e6eb1a11.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/cienciasBCSApp/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/cienciasBCSApp/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/cienciasBCSApp/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/cienciasBCSApp/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "1cfd2bc34ca24ed176c4f292c043ab0f",
+    "url": "/cienciasBCSApp/static/media/slide-1.1cfd2bc3.jpg"
+  },
+  {
+    "revision": "1cfd2bc34ca24ed176c4f292c043ab0f",
+    "url": "/cienciasBCSApp/static/media/slide-2.1cfd2bc3.jpg"
+  },
+  {
+    "revision": "1cfd2bc34ca24ed176c4f292c043ab0f",
+    "url": "/cienciasBCSApp/static/media/slide-3.1cfd2bc3.jpg"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/cienciasBCSApp/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/cienciasBCSApp/static/media/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/cienciasBCSApp/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/cienciasBCSApp/static/media/themify.e23a7dca.ttf"
+  }
+]);
